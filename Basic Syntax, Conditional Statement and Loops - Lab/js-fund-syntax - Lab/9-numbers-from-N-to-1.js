72563b3c4e360eef10feb5cce4ca1179987c6e8e
Dimitar Damianov <114077179+dimitargb@users.noreply.github.com>
2023-12-05 16:59:55 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statement and Loops - Lab/js-fund-syntax - Lab/9-numbers-from-N-to-1.js
@@ -0,0 +1,10 @@
+function solve(number) {
+    let currentNumber = number;
+
+    while(currentNumber >= 1) {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
+
+solve(5);
---
+++ Basic Syntax, Conditional Statement and Loops - Lab/js-fund-syntax - Lab/introduce.js
@@ -0,0 +1,9 @@
+function introduce(name, age) {
+    // let introduction = 'Hello my name is ' + name + ' and I\'m ' + age + ' years old!';
+    let introduction = `Hello my name is ${name} and I'm ${age} years old`;
+
+    console.log(introduction);
+}
+
+introduce('Pesho', 20);
+introduce('Gosho', 21);
